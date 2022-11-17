4a01f274b6115d168504d79bb830b57cffd5c498
CLKMITL07 <CLKMITL07@CLKMITL07.CLKMITL07>
2022-11-18 06:18:25 +0700

    ok

--- templates/main.js
+++ templates/main.js
@@ -1,10 +1,13 @@
 let keyword = "blackpink";
+// let edit_save = document.getElementById("img");
+// edit_save.src = "./result/wc-all.png"; 
 
 //search
 function compute(select) {
     keyword = document.getElementById("simple-search").value;
     var noOfTweet=10;
     eel.input(keyword, noOfTweet, select);
+    // edit(select);
     location.reload();
 }
 
@@ -12,9 +15,11 @@ function compute(select) {
 function computeButton(select) {
     var noOfTweet=10;
     eel.input(keyword, noOfTweet, select);
+    // edit(select);
     location.reload();
 }
 
+//reload img
 function updateImg() {
     var source = '/result/wc-all.png',
         timestamp = (new Date()).getTime(),
@@ -22,3 +27,14 @@ function updateImg() {
     document.getElementById("img").src = newUrl;
     setTimeout(update, 1000);
 }
+
+// function edit(select)
+// {   
+//     var inputs = document.myform;
+//     inputs[4] = "./result/wc-all.png";
+//     inputs[1] = "./result/wc-pos.png";
+//     inputs[3] = "./result/wc-neu.png";
+//     inputs[2] = "./result/wc-neg.png";
+
+//     edit_save.src = inputs[select];                              
+// }
